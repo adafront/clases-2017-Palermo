4619e7cde58bc3339b505cff31dc34319b0fb999
Estefania Aguilar <Estefania>
2017-11-08 15:44:22 -0300

    updateo archivos de la clase 07-11 + ejemplo de setAttribute

--- 07-11/dom/main.js
+++ 07-11/dom/main.js
@@ -8,13 +8,29 @@ var primeraCaja = document.getElementById('primeraCaja');
 primeraCaja.innerHTML = '<h1>Hola mundo</h1>';
 
 var caja = document.createElement('div');
-var contenido = document.createTextNode('Hola');
+var contenido = document.createTextNode('Nueva caja');
+
+//agregar un nodo dentro de otro
 caja.appendChild(contenido);
 
-caja.setAttribute('class','caja naranja');
+//caja.setAttribute('class','caja naranja');
+caja.className = "caja naranja";
+//caja.style.backgroundColor = "orange";
+
 
 var contenedor = document.getElementById('contenedor');
 contenedor.appendChild(caja);
+
+primeraCaja.style.backgroundColor = "blue";
+
+//movemos a la caja a la posicion deseada
+//contenedor.insertBefore(caja,cajas[2]);
+
+contenedor.replaceChild(caja,cajas[2]);
+contenedor.removeChild(cajas[3]);
+n                                                                                    
+
+
 
 
 
---
+++ 08-11/setAttribute.js
@@ -0,0 +1,5 @@
+function myFunction() {
+    document.getElementsByTagName("INPUT")[0].setAttribute("type", "button"); 
+};
+
+
